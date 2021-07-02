03cdd520f9d7988b72411efe8e301f413b16bc8b
Alexander <alexkobayashi128@gmail.com>
2021-07-02 14:15:40 -0700

    added function that displays the array and each index on the DOM

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const player = (playerLetter, turn) => {
     const getLetter = () => playerLetter;
     const playerTurn = () => turn;
-    return {getLetter, turn};
+    return {getLetter, playerTurn};
 };
 
 const GameBoard = (() => {
-    let gameBoard = [];
-    const player1 = player('x', true);
-    const player2 = player('o', false);
+    let gameBoard = ['O', 'X', 'O', 'O','O', 'X', 'O', 'O', 'X'];
+        for(i = 0; gameBoard.length > i; i++){
+                document.getElementById(i).textContent = gameBoard[i];
+        };
+    
 })();
 
-const GameFlow = () => {
-
-};
-
-const DisplayGameBoard =  (() => {
-
+const GameFlow = (() => {
+    const player1 = player('x', true);
+    const player2 = player('o', false);
+    document.querySelectorAll('td').forEach((element) => {
+        element.addEventListener('click', () =>{
+            element.textContent = 'X';
+        });
+    });
 })();
